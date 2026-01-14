4cf917d894d3e1061ea2b61d93924583d7d87f2c
Amardipy10 <amardipy10@gmail.com>
2026-01-14 22:01:51 +0530

    correction

--- subarray.cpp
+++ subarray.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include <climits>
 using namespace std;
+//optimal
 int subarray(vector<int>& v){
     int n=v.size();
     int curr_sum=0;
@@ -16,4 +17,19 @@ int subarray(vector<int>& v){
     }
     return max_sum;
     
+}
+
+//brute
+int maxSubarrayBrute(vector<int>& v) {
+    int n = v.size();
+    int max_sum = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        int curr_sum = 0;
+        for (int j = i; j < n; j++) {
+            curr_sum += v[j];
+            max_sum = max(max_sum, curr_sum);
+        }
+    }
+    return max_sum;
 }
