20f60d9aef67fcff02c260cad1025c399cfc3769
Amardipy10 <amardipy10@gmail.com>
2026-01-19 16:01:25 +0530

    recursion

---
+++ sum_n_rec.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int sum_of_n(int n){
+    if(n==0){
+        return 0;
+    }
+    return (n+sum_of_n(n-1));
+}
+
+int main(){
+    int ans=sum_of_n(5);
+    cout<<ans;
+}
