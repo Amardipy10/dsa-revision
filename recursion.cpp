ee227ec05c583b794dc14a0db476c51918dcd4a7
Amardipy10 <amardipy10@gmail.com>
2026-01-19 16:16:08 +0530

    recursion

--- recursion.cpp
+++ recursion.cpp
@@ -12,15 +12,29 @@ using namespace std;
 //     cout<<ans;
 // }
 
-int factorial(int n){
-    if(n==0){
-        return 1;
-    }
-    else{
-    return (n*factorial(n-1));
-    }
-}
-int main(){
-    int ans=factorial(5);
-    cout<<ans;
-}
+// int factorial(int n){
+//     if(n==0){
+//         return 1;
+//     }
+//     else{
+//     return (n*factorial(n-1));
+//     }
+// }
+// int main(){
+//     int ans=factorial(5);
+//     cout<<ans;
+// }
+
+// int fibonacci(int n){
+//     if(n==1 || n==0){
+//         return n;
+//     }
+//     else{
+//         return fibonacci(n-1)+fibonacci(n-2);
+//     }
+// }
+
+// int main(){
+//     int ans=fibonacci(2);
+//     cout<<ans;
+// }
