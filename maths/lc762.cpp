6546da3e673a49e2d484878fb2b2342f8eb1b370
Amardipy10 <amardipy10@gmail.com>
2026-02-21 13:46:05 +0530

    more-maths

--- maths/lc762.cpp
+++ maths/lc762.cpp
@@ -34,3 +34,31 @@ public:
         return result;
     }
 };
+
+//optimal
+class Solution {
+public:
+
+    bool isPrime(int num){
+        if(num < 2) return false;
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    int countPrimeSetBits(int left, int right) {
+        int result = 0;
+
+        for(int i = left; i <= right; i++){
+            int count1 = __builtin_popcount(i);
+
+            if(isPrime(count1)){
+                result++;
+            }
+        }
+
+        return result;
+    }
+};
