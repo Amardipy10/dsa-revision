9c2cce9f867d4b93262c65e87c2282fc7a5887dd
Amardipy10 <amardipy10@gmail.com>
2026-02-25 02:14:32 +0530

    Qs

---
+++ maths/arrcoins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count=0;
+        int i=1;
+        while(n>0 && n>=i){
+            n=n-i;
+            i=i+1;
+            count++;
+        }
+        return count;
+    }
+};
