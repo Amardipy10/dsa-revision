0f735971d7ffff9f296e082c078a334e494e6c20
Amardipy10 <amardipy10@gmail.com>
2026-04-08 13:17:39 +0530

    more qs

---
+++ leetcode/lc3653.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        long long mod = 1e9 + 7;
+
+        for (const auto& q : queries) {
+            int l = q[0];
+            int r = q[1];
+            int k = q[2];
+            int v = q[3];
+
+            for (int idx = l; idx <= r; idx += k) {
+                nums[idx] = (1LL * nums[idx] * v) % mod;
+            }
+        }
+
+        int result = 0;
+        for (int val : nums) {
+            result ^= val;
+        }
+
+        return result;
+    }
+};
